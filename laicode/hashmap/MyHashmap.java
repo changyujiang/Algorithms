b57d80be201ce2cc5f8bc864f79548667d7d1691
changyujiang <changyu.jiang86@outlook.com>
2017-11-05 22:55:25 -0800

    implement hashmap

---
+++ laicode/hashmap/MyHashmap.java
@@ -0,0 +1,8 @@
+package laicode.hashmap;
+
+public class MyHashmap<K,V> {
+	
+	public V put(K key){
+		
+	}
+}
---
+++ laicode/hashmap/Node.java
@@ -0,0 +1,24 @@
+package laicode.hashmap;
+
+public class Node<K,V> {
+	final K key;
+	V value;
+	Node<K,V> next;
+	
+	public Node(K key, V value) {
+		this.key = key;
+		this.value = value;
+	}
+	
+	public K getKey() {
+		return key;
+	}
+	
+	public V getValue() {
+		return value;
+	}
+	
+	public void setValue(V value) {
+		this.value = value;
+	}
+}
