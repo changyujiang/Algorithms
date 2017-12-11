1bdf87d922c6afe969b3cfecb3b874a73d28ba66
changyujiang <changyu.jiang86@outlook.com>
2017-12-10 23:03:01 -0800

    add

--- laicode/test/Test.java
+++ laicode/test/Test.java
@@ -1,12 +1,13 @@
 package laicode.test;
 
-import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Test {
 	public static void main (String[] args) {
-		String input = "robob";
-		// robob text
-		// 01234 index
-		System.out.println(input.substring(1, 2));
+		 int left = 0;
+	        int right = Integer.MAX_VALUE;
+	        int mid = (left + right) / 2;
+	        System.out.println(right);
 	}
 }
